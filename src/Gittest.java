76233aef5df18719308d520119cb888ae2dbe620
suchi-devel <52340673+suchi-devel@users.noreply.github.com>
2020-04-05 02:11:50 +0530

    Update Gittest.java
    commit new sys  out to newclass

--- src/Gittest.java
+++ src/Gittest.java
@@ -4,6 +4,8 @@ public class Gittest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("this is isj");
+		
+System.out.println("this is  new  isj");
 	}
 
 }
