9918000176205f4ecf585a8db70b6d14c0d4911f
suchi-devel <suchitrakar2008@gmail.com>
2020-05-03 20:57:35 +0530

    This is sit merge commit

--- src/thirdgittest.java
+++ src/thirdgittest.java
@@ -4,6 +4,7 @@ public class thirdgittest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is 3rd new  isj");
+		System.out.println("this is 3rd new sit mergetest");
 	}
 
 }
