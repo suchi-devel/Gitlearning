439c4f6bb4b7ba14789a793466a119985e795d62
suchi-devel <suchitrakar2008@gmail.com>
2020-05-24 22:13:31 +0530

    adding thirdgittest java to test branch

--- src/Thirdgittest.java
+++ src/Thirdgittest.java
@@ -1,10 +1,12 @@
 
-public class thirdgittest {
+public class Thirdgittest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is 3rd new  isj");
 		System.out.println("this is 3rd new sit mergetest");
+		System.out.println("this is 3rd new test branchupdate");
+		
 	}
 
 }
