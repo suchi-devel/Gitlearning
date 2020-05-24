461b9b30c40596f3b78d56b509faeddb7bf2ee6e
suchi-devel <suchitrakar2008@gmail.com>
2020-05-24 21:58:26 +0530

    commiting 2nd sit test to sit branch

---
+++ src/SecondsitTest.java
@@ -0,0 +1,10 @@
+
+public class SecondsitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is 3rd new sit mergetest");
+		System.out.println("this is 3rd new sit rebase test");
+	}
+
+}
